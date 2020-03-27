1487974124fcb3823ef3c9195366356fb14f0f68
Abdulrahman Ayad <abdul.5000@hotmail.co.uk>
2020-03-27 11:59:01 -0600

    Added error checking for the edit feature

--- PPMT/Edit.xaml.cs
+++ PPMT/Edit.xaml.cs
@@ -70,7 +70,11 @@ namespace PPMT
 
                 oldBubble = new ScatterPoint(t.Item1, t.Item2, project.value);
 
-                n.Text = project.pName;
+                name.Text = project.pName;
+                sponsor.Text = project.pSponsor;
+                date.Text = project.rDate;
+                deadline.Text = project.pDeadline;
+                savings.Text = project.pSavings.ToString();
                 edit = this;
                 tup = t;
 
@@ -141,7 +145,11 @@ namespace PPMT
 
             Main.Content = null;
 
-            n.Text = proj.pName;
+            name.Text = proj.pName;
+            sponsor.Text = proj.pSponsor;
+            date.Text = proj.rDate;
+            deadline.Text = proj.pDeadline;
+            savings.Text = proj.pSavings.ToString();
             project = proj;
 
             val1.Value = proj.value;
@@ -209,84 +217,153 @@ namespace PPMT
         private void saveButtonClicked(object sender, RoutedEventArgs e)
         {
 
-            int index = 0;
-
-            for (int i = 0; i < PPMT.BubbleGraphUserControl.BubbleGraph.JSONList.Count; i++)
+            if (savings.Text == "" && name.Text == "" && sponsor.Text == "")
             {
-                if (PPMT.BubbleGraphUserControl.BubbleGraph.JSONList[i].pName == project.pName)
+                MessageBox.Show("Please fill out the following: Project Name, Project Sponsor, Savings");
+            }
+            else if (savings.Text == "" && name.Text == "")
+            {
+                MessageBox.Show("Please fill out the following: Project Name, Savings");
+            }
+            else if (savings.Text == "" && sponsor.Text == "")
+            {
+                MessageBox.Show("Please fill out the following: Project Sponsor, Savings");
+            }
+            else if (name.Text == "" && sponsor.Text == "")
+            {
+                MessageBox.Show("Please fill out the following: Project Name, Project Sponsor");
+            }
+            else if (savings.Text == "")
+            {
+                MessageBox.Show("Please fill out the following: Savings");
+            }
+            else if (name.Text == "")
+            {
+                MessageBox.Show("Please fill out the following: Project Name");
+            }
+            else if (sponsor.Text == "")
+            {
+                MessageBox.Show("Please fill out the following: Project Sponsor");
+            }
+            else
+            {
+
+
+                if (name.Text.Length >= 5 && sponsor.Text.Length >= 5 && savings.Text.Length <= 19)
                 {
-                    index = i;
-                    break;
+
+                    int index = 0;
+
+                    for (int i = 0; i < PPMT.BubbleGraphUserControl.BubbleGraph.JSONList.Count; i++)
+                    {
+                        if (PPMT.BubbleGraphUserControl.BubbleGraph.JSONList[i].pName == project.pName)
+                        {
+                            index = i;
+                            break;
+                        }
+                    }
+
+                    PPMT.BubbleGraphUserControl.BubbleGraph.deleteProject(oldBubble, tup, project);
+
+                    if (tG.IsSelected)
+                    {
+
+                        project.projectCategory = "Transformative Growth";
+
+                    }
+                    else if (qOT.IsSelected)
+                    {
+
+                        project.projectCategory = "Quality of Talent";
+
+                    }
+                    else if (mAK.IsSelected)
+                    {
+
+                        project.projectCategory = "Measurement and KPI";
+
+                    }
+                    else if (oNA.IsSelected)
+                    {
+
+                        project.projectCategory = "ONA";
+
+                    }
+                    else if (cC.IsSelected)
+                    {
+
+                        project.projectCategory = "Consolidation/Cost";
+
+                    }
+                    else if (other.IsSelected)
+                    {
+
+                        project.projectCategory = "Other";
+
+                    }
+
+                    project.pName = name.Text;
+
+                    project.pSponsor = sponsor.Text;
+
+                    project.rDate = date.Text;
+
+                    project.pDeadline = deadline.Text;
+
+                    project.pSavings = ulong.Parse(savings.Text);
+
+                    project.value = val1.Value;
+
+                    project.transformativeG = val2.Value;
+
+                    project.hrpriority = val3.Value;
+
+                    project.risk = val4.Value;
+
+                    project.resource = val5.Value;
+
+                    project.data = val6.Value;
+
+                    project.vendors = val7.Value;
+
+                    project.sponsorship = val8.Value;
+
+                    project.implementation = val9.Value;
+
+                    PPMT.BubbleGraphUserControl.BubbleGraph.AddNewProject(project);
+
+                    PPMT.BubbleGraphUserControl.BubbleGraph.JSONList[index] = project;
+
+                    PPMT.MainWindow.mainAccess.createSlideProj();
+
+                    string docPath = Directory.GetCurrentDirectory();
+
+                    string convertedJson = JsonConvert.SerializeObject(PPMT.BubbleGraphUserControl.BubbleGraph.JSONList, Formatting.Indented);
+
+                    File.WriteAllText((System.IO.Path.Combine(docPath, "projects.json")), convertedJson);
+
+                    edit.Close();
                 }
-            }
+                else if (sponsor.Text.Length < 5 && name.Text.Length < 5)
+                {
+                    MessageBox.Show("The minimum length for the Project Sponsor and Project Name must include at least 5 characters");
+                }
+                else if (name.Text.Length < 5)
+                {
+                    MessageBox.Show("The minimum length for the Project Name must include at least 5 characters");
+                }
+                else if (sponsor.Text.Length < 5)
+                {
+                    MessageBox.Show("The minimum length for the Project Sponsor must include at least 5 characters");
+                }
+                else if (savings.Text.Length > 19)
+                {
+                    MessageBox.Show("The Savings value is too high");
+                }
 
-            PPMT.BubbleGraphUserControl.BubbleGraph.deleteProject(oldBubble, tup, project);
 
-            if (tG.IsSelected)
-            {
-
-                project.projectCategory = "Transformative Growth";
-
-            } else if (qOT.IsSelected)
-            {
-
-                project.projectCategory = "Quality of Talent";
-
-            } else if (mAK.IsSelected)
-            {
-
-                project.projectCategory = "Measurement and KPI";
-
-            } else if (oNA.IsSelected)
-            {
-
-                project.projectCategory = "ONA";
-
-            } else if (cC.IsSelected)
-            {
-
-                project.projectCategory = "Consolidation/Cost";
-
-            } else if (other.IsSelected)
-            {
-
-                project.projectCategory = "Other";
 
             }
-
-            project.pName = n.Text;
-
-            project.value = val1.Value;
-
-            project.transformativeG = val2.Value;
-
-            project.hrpriority = val3.Value;
-
-            project.risk = val4.Value;
-
-            project.resource = val5.Value;
-
-            project.data = val6.Value;
-
-            project.vendors = val7.Value;
-
-            project.sponsorship = val8.Value;
-
-            project.implementation = val9.Value;
-
-            PPMT.BubbleGraphUserControl.BubbleGraph.AddNewProject(project);
-
-            PPMT.BubbleGraphUserControl.BubbleGraph.JSONList[index] = project;
-
-            PPMT.MainWindow.mainAccess.createSlideProj();
-
-            string docPath = Directory.GetCurrentDirectory();
-
-            string convertedJson = JsonConvert.SerializeObject(PPMT.BubbleGraphUserControl.BubbleGraph.JSONList, Formatting.Indented);
-
-            File.WriteAllText((System.IO.Path.Combine(docPath, "projects.json")), convertedJson);
-
-            edit.Close();
         }
 
         private void deleteButtonClicked(object sender, RoutedEventArgs e)
@@ -317,6 +394,29 @@ namespace PPMT
 
             edit.Close();
 
+        }
+
+        private void MaskNumericInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !TextIsNumeric(e.Text);
+        }
+
+        private void MaskNumericPaste(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                string input = (string)e.DataObject.GetData(typeof(string));
+                if (!TextIsNumeric(input)) e.CancelCommand();
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private bool TextIsNumeric(string input)
+        {
+            return input.All(c => Char.IsDigit(c) || Char.IsControl(c));
         }
 
     }
